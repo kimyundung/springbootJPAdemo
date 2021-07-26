c44e5576ba3225fe0a15aae84685ad6032f0e1e8
kimyundung <kimyundung@naver.com>
2021-07-26 14:43:44 +0800

    查询策略设置

--- springbootjpademo/src/main/java/com/stone/SpringbootjpademoApplication.java
+++ springbootjpademo/src/main/java/com/stone/SpringbootjpademoApplication.java
@@ -2,8 +2,11 @@ package com.stone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.query.QueryLookupStrategy;
 
 @SpringBootApplication
+@EnableJpaRepositories(queryLookupStrategy = QueryLookupStrategy.Key.CREATE_IF_NOT_FOUND)
 public class SpringbootjpademoApplication {
 
 	public static void main(String[] args) {
