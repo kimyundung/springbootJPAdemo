13528eb40b5f5a6b9c256f86c7792cf9cc47ccdc
kimyundung <kimyundung@naver.com>
2021-07-27 19:46:52 +0800

    springbootjpademo05 Initializr

---
+++ springbootjpademo05/src/test/java/com/stone/springbootjpademo05/Springbootjpademo05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.springbootjpademo05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootjpademo05ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
