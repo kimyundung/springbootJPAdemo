0fa9e80481b37f3a07a8e49bc3023d442adef139
kimyundung <kimyundung@naver.com>
2021-07-29 11:28:20 +0800

    @Query

---
+++ springbootjpademo05/src/main/java/com/stone/repository/UserRepository.java
@@ -0,0 +1,88 @@
+package com.stone.repository;
+
+import com.stone.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    /**
+     * 通过query注解根据name查询user信息
+     */
+    @Query("From User where name=:name")
+    List<User> findByQuery(@Param("name") String nameParam);
+
+    /**
+     * 案例 1
+     * Repository 的查询方法上声明一个注解，这里就是 @Query 注解标注的地方。
+     */
+    @Query("select u from User u where u.email=?1")
+    List<User> findByEmail(String email);
+
+    /**
+     * 案例 2
+     * LIKE 查询，注意 firstname 不会自动加上“%”关键字。
+     */
+    @Query("select u from User u where u.name like %?1")
+    List<User> findByNameEndsWith(String name);
+
+    /**
+     * 案例 3
+     * 直接用原始 SQL，nativeQuery = true 即可。
+     * 注意：nativeQuery 不支持直接 Sort 的参数查询。
+     */
+    @Query(value = "SELECT * FROM USER WHERE ADDRESS = ?1", nativeQuery = true)
+    List<User> findByAddress(String address);
+
+    /**
+     * 案例 4
+     * 下面是****nativeQuery 的排序错误的写法，会导致无法启动。
+     */
+//    @Query(value = "select * from user where name = ?1 and email = ?2", nativeQuery = true)
+//    List<User> findByNameAndEmail(String name, String email, Sort sort);
+
+    /**
+     * 案例 5
+     * nativeQuery 排序的正确写法。
+     *
+     * 调用的地方写法last_name是数据里面的字段名，不是对象的字段名
+     * repository.findByFirstName("jackzhang","last_name");
+     */
+    @Query(value = "select * from user where name = ?1 and email = ?2 order by ?3", nativeQuery = true)
+    List<User> findByNameAndEmail(String name, String email, String sort);
+
+    /**
+     * 案例 6
+     * Sort and JpaSort 的使用，它可以进行排序。
+     */
+    @Query("select u from User u where u.name like %?1")
+    List<User> findByAndSort(String name, Sort sort);
+
+    @Query("select u.id, LENGTH(u.name) as fn_len from User u where u.name like %?1")
+    List<Object[]> findByAsArrayAndSort(String name,Sort sort);
+
+    /**
+     * 案例 7
+     * Query 的分页
+     * 直接用 Page 对象接受接口, 参数直接用 Pageable 的实现类即可.
+     */
+    @Query(value = "select u from User u where u.name = ?1")
+    Page<User> findByName (String name, Pageable pageable);
+
+    /**
+     * 案例 8
+     * Query 的分页
+     * @Query 对原生 SQL 的分页支持, 并不是特别友好, 因为这种写法比较"hacker", 可能随着版本的不同有所变化
+     * 这里需要注意：这个注释 / #pageable# / 必须有
+     */
+    @Query( value = "select * from user where name like %?%1 /* #pageable# */",
+            countQuery = "select count(*) from user where name like %?%1",
+            nativeQuery = true)
+    Page<User> findByNameLike(String name, Pageable pageable);
+}
--- springbootjpademo05/src/test/java/com/stone/UserRepositoryQueryTest.java
+++ springbootjpademo05/src/test/java/com/stone/UserRepositoryQueryTest.java
@@ -1,24 +1,158 @@
 package com.stone;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stone.entity.User;
-import com.stone.repository.UserDtoRepository;
+import com.stone.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.JpaSort;
 
+import java.util.Arrays;
 import java.util.List;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryQueryTest {
     @Autowired
-    private UserDtoRepository userDtoRepository;
+    private UserRepository userRepository;
 
     @Test
     public void test1(){
         //调用上面的方法查看结果
-        List<User> userList = userDtoRepository.findByQuery("stone");
+        List<User> userDtoList = userRepository.findByQuery("stone");
+        System.out.println(userDtoList);
+    }
+
+    @Test
+    public void test2(){
+        // 案例 1
+//        List<User> userList = userRepository.findByEmail("stone@stone");
+//        System.out.println(userList);
+
+        // 案例 2
+//        List<User> userList = userRepository.findByNameEndsWith("2");
+//        System.out.println(userList);
+
+        // 案例 3
+//        List<User> userList = userRepository.findByAddress("shanghai");
+//        System.out.println(userList);
+
+        // 案例 4 ERROR
+//        List<User> userList = userRepository.findByNameAndEmail("stone", "stone@stone", Sort.by(Sort.Direction.ASC, "name"));
+//        System.out.println(userList);
+
+        // 案例 5
+//        List<User> userList = userRepository.findByNameAndEmail("STONE", "STONE@STONE", "name");
+//        System.out.println(userList);
+    }
+
+    @Test
+    public void test3(){
+        // 案例 6
+        List<User> userList = userRepository.findByAndSort("2", Sort.by(Sort.Direction.DESC, "name"));
         System.out.println(userList);
+        System.out.println("-------------------------------------");
+
+        // error
+//        List<User> userList1 = userRepository.findByAndSort("2", Sort.by("LENGTH(name)"));
+//        List<User> userList1 = userRepository.findByAndSort("2", Sort.by(Sort.Direction.ASC, "LENGTH(name)"));
+//        System.out.println(userList1);
+//        System.out.println("-------------------------------------");
+
+        List<User> userList2 = userRepository.findByAndSort("2", JpaSort.unsafe("LENGTH(name)"));
+        System.out.println(userList2);
+        System.out.println("-------------------------------------");
+
+        List<Object[]> objects = userRepository.findByAsArrayAndSort("2", Sort.by("fn_len"));
+        for(Object[] o : objects){
+            System.out.println(Arrays.toString(o));
+        }
+    }
+
+    ObjectMapper ob = new ObjectMapper();
+    @Test
+    public void test4() throws JsonProcessingException {
+        // 案例 7
+        Page<User> userPage = userRepository.findByName("stone", PageRequest.of(1, 2));
+        System.out.println(ob.writeValueAsString(userPage));
+    }
+
+    @Test
+    public void test5() throws JsonProcessingException {
+        // 案例 8
+        Page<User> userPage = userRepository.findByNameLike("stone", PageRequest.of(1, 2));
+        System.out.println(ob.writeValueAsString(userPage));
+        /* ????????????????????????????????
+        {
+            "content": [
+                {
+                    "id": 32,
+                    "name": "stoneUpdate2",
+                    "email": "stone@stone",
+                    "sex": null,
+                    "address": null
+                },
+                {
+                    "id": 33,
+                    "name": "stone2",
+                    "email": "stone2@stone",
+                    "sex": null,
+                    "address": "shanghai"
+                },
+                {
+                    "id": 34,
+                    "name": "stone",
+                    "email": "stone@stone",
+                    "sex": null,
+                    "address": null
+                },
+                {
+                    "id": 54,
+                    "name": "stone",
+                    "email": "stone@stone",
+                    "sex": "women",
+                    "address": "yancheng"
+                },
+                {
+                    "id": 56,
+                    "name": "stone",
+                    "email": "stone3@stone",
+                    "sex": "man",
+                    "address": "yancheng"
+                }
+            ],
+            "pageable": {
+                "sort": {
+                    "unsorted": true,
+                    "sorted": false,
+                    "empty": true
+                },
+                "offset": 2,
+                "pageNumber": 1,
+                "pageSize": 2,
+                "paged": true,
+                "unpaged": false
+            },
+            "last": false,
+            "totalElements": 5,
+            "totalPages": 3,
+            "size": 2,
+            "number": 1,
+            "sort": {
+                "unsorted": true,
+                "sorted": false,
+                "empty": true
+            },
+            "first": false,
+            "numberOfElements": 5,
+            "empty": false
+        }
+        */
     }
 }
